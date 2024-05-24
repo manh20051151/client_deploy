3fe7e8728aa31fad5f208f63032ef16311126325
manh20051151 <nguyenvietmanh1409@gmail.com>
2024-05-24 19:46:52 +0700

    fix url

--- src/axios.js
+++ src/axios.js
@@ -21,7 +21,7 @@ instance.interceptors.request.use(function (config){
 instance.interceptors.response.use(function (response){
     return response.data
 },function(error){
-    return error.response.data
+    return error?.response?.data
 })
 
 export default instance
